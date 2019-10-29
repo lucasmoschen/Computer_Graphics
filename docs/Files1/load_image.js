2deda0af628f43534909b5bcda197367646de0c9
Lucas Moschen <lucas.machadomoschen@gmail.com>
2019-10-28 23:11:22 -0300

    Adding demonstration to the site

--- docs/Files1/load_image.js
+++ docs/Files1/load_image.js
@@ -204,6 +204,36 @@ try{
 		}
 		beta.src = "../Files1/img/"+betaImages[i];
 	}
+
+	//Adding image for demonstration
+	
+	var canvasBcell = document.getElementById("Bcell");
+	var ctxBcell = canvasBcell.getContext("2d");
+
+	var imageCell = new Image();
+
+	imageCell.crossOrigin = '';
+
+	imageCell.src = "118.png"
+
+	//imageCell.src = "https://i.ibb.co/MkJS4GP/118.png"
+
+	imageCell.onload = function() {
+		ctxBcell.drawImage(imageCell,100,0,500,500);
+		imageData = ctxBcell.getImageData(100,0,500,500);
+		hist = histogram(imageData);
+		return hist;
+	};
+
+	histogramMatch = function(matching){
+		imageData = ctxBcell.getImageData(100,0,500,500);
+		for(let i = 0; i < imageData.data.length; i+=4){
+			imageData.data[i] = matching[imageData.data[i]];
+			imageData.data[i+1] = matching[imageData.data[i+1]];
+			imageData.data[i+2] = matching[imageData.data[i+2]]; 
+		}
+		ctxBcell.putImageData(imageData,100,0);
+	}
 }
 catch(err){
 	console.log(err);
