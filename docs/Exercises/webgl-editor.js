157f68b5e8fd454c2e2073fbb46a4875a78a39ab
Lucas Moschen <lucas.machadomoschen@gmail.com>
2019-11-11 16:17:17 -0300

    Web GL. The matrices are done.

--- docs/Exercises/webgl-editor.js
+++ docs/Exercises/webgl-editor.js
@@ -1,87 +1,225 @@
-// Sets up the the context and starts the rendering. 
+//--------------------------Canvas---------------------------//
+ var canvas = document.getElementById('glCanvas');
+ gl = canvas.getContext('experimental-webgl');
 
-function main() {
-  const canvas = document.querySelector("#glCanvas");
-  // Initialize the GL context
-  const gl = canvas.getContext("webgl");
+ var sliderX = document.getElementById("myRangeX");
+ var outputX = document.getElementById("demoX");
 
-  // Only continue if WebGL is available and working
-  if (gl === null) {
-    alert("Unable to initialize WebGL. Your browser or machine may not support it.");
-    return;
-  }
+ outputX.innerHTML = sliderX.value; // Display the default slider value
 
-  // Set clear color to black, fully opaque
-  gl.clearColor(255, 0.0, 0.0, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
+ // Update the current slider value (each time you drag the slider handle)
+ sliderX.oninput = function() {
+   outputX.innerHTML = this.value;
+ } 
 
-  var vertices = [-0.5, 0.5, 0.0, -0.5, -0.5, 0.0, 0.0, -0.5,0.0];
-  var indices = [0,1,2];
+ var sliderY = document.getElementById("myRangeY");
+ var outputY = document.getElementById("demoY");
 
-  var vertex_buffer = gl.createBuffer(); // Create a new buffer object
+ outputY.innerHTML = sliderY.value; // Display the default slider value
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer); // Bind an empty array buffer to it
-  
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW); // Pass the vertices data to the buffer
+ // Update the current slider value (each time you drag the slider handle)
+ sliderY.oninput = function() {
+   outputY.innerHTML = this.value;
+ } 
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, null); // Unbind the buffer
+ var sliderZ = document.getElementById("myRangeZ");
+ var outputZ = document.getElementById("demoZ");
 
-  var Index_Buffer = gl.createBuffer();
+ outputZ.innerHTML = sliderZ.value; // Display the default slider value
 
-  // Bind appropriate array buffer to it
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
+ // Update the current slider value (each time you drag the slider handle)
+ sliderZ.oninput = function() {
+   outputZ.innerHTML = this.value;
+ } 
 
-  // Pass the vertex data to the buffer
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
-  
-  // Unbind the buffer
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+//-----------------Geometry of the object-------------------//
 
-  var vertCode =
-    'attribute vec3 coordinates;' + 
-    'void main(void) {' + ' gl_Position = vec4(coordinates, 1.0);' + '}'; //source code
+ var vertices = [
+    -1,-1,-1,    1,-1,-1,    1, 1,-1,   -1, 1,-1,           //faces 
+    -1,-1, 1,    1,-1, 1,    1, 1, 1,   -1, 1, 1,
+    -1,-1,-1,   -1, 1,-1,   -1, 1, 1,   -1,-1, 1,
+     1,-1,-1,    1, 1,-1,    1, 1, 1,    1,-1, 1,
+    -1,-1,-1,   -1,-1, 1,    1,-1, 1,    1,-1,-1,
+    -1, 1,-1,   -1, 1, 1,    1, 1, 1,    1, 1,-1, 
+ ];
 
-  var vertShader = gl.createShader(gl.VERTEX_SHADER);
+ var colors = [  
+    0,1,1, 0,1,1, 0,1,1, 0,1,1,
+    0,0,1, 0,0,1, 0,0,1, 0,0,1,
+    1,0,1, 1,0,1, 1,0,1, 1,0,1,
+    1,0,0, 1,0,0, 1,0,0, 1,0,0,
+    0,1,0, 0,1,0, 0,1,0, 0,1,0,
+    1,1,0, 1,1,0, 1,1,0, 1,1,0,
+ ];
 
-  gl.shaderSource(vertShader, vertCode); //Attach vertex shader source code
+ var indices = [
+    0,1,2, 0,2,3, 4,5,6, 4,6,7,
+    8,9,10, 8,10,11, 12,13,14, 12,14,15,
+    16,17,18, 16,18,19, 20,21,22, 20,22,23 
+ ];
 
-  gl.compileShader(vertShader);
+//-----------------------BUFFERS--------------------------//
+ var vertex_buffer = gl.createBuffer ();
+ gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+ gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
-  var fragCode = 
-     'void main(void) {' + 'gl_FragColor = vec4(1.0, 0.5, 0.0, 1);' + '}'; //source code
+ var color_buffer = gl.createBuffer ();
+ gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+ gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
 
-  var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+ var index_buffer = gl.createBuffer ();
+ gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
+ gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
 
-  gl.shaderSource(fragShader, fragCode);
+//----------------------------SHADERS-------------------------------//
 
-  gl.compileShader(fragShader);
+ var vertCode = 'attribute vec3 position;'+
+    'uniform mat4 Pmatrix;'+   //create for this shader a matrix 4 for 4, to do the operations. 
+    'uniform mat4 Vmatrix;'+
+    'uniform mat4 Mmatrix;'+
+    'attribute vec3 color;'+   
+    'varying vec3 vColor;'+
 
-  var shaderProgram = gl.createProgram(); // Create a shader program to store combined shader program
+    //the position is made about multiplying the matrix to the position. Note we do w = 1.  
+    'void main(void) { '+
+       'gl_Position = Pmatrix*Vmatrix*Mmatrix*vec4(position, 1.);'+
+       'vColor = color;'+
+    '}';
 
-  gl.attachShader(shaderProgram, vertShader); // Attach a vertex shader
-  
-  gl.attachShader(shaderProgram, fragShader);
+ var fragCode = 'precision mediump float;'+
+    'varying vec3 vColor;'+
+    'void main(void) {'+
+       'gl_FragColor = vec4(vColor, 1.);'+
+    '}';
 
-  gl.linkProgram(shaderProgram); // Link both programs
+//------------------------------CREATE SHADERS ---------------------------------//
+ var vertShader = gl.createShader(gl.VERTEX_SHADER);
+ gl.shaderSource(vertShader, vertCode);
+ gl.compileShader(vertShader);
 
-  gl.useProgram(shaderProgram); // Use the combined shader program object
+ var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+ gl.shaderSource(fragShader, fragCode);
+ gl.compileShader(fragShader);
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer); //Bind vertex buffer object
+ var shaderProgram = gl.createProgram();
+ gl.attachShader(shaderProgram, vertShader);
+ gl.attachShader(shaderProgram, fragShader);
+ gl.linkProgram(shaderProgram);
 
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
+//Associating values with the Locatation in the Program
+ var Pmatrix = gl.getUniformLocation(shaderProgram, "Pmatrix");
+ var Vmatrix = gl.getUniformLocation(shaderProgram, "Vmatrix");
+ var Mmatrix = gl.getUniformLocation(shaderProgram, "Mmatrix");
 
-  var coord = gl.getAttribLocation(shaderProgram, "coordinates"); //Get the attribute location
+ gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+ var position = gl.getAttribLocation(shaderProgram, "position");
+ gl.vertexAttribPointer(position, 3, gl.FLOAT, false,0,0) ;
+ gl.enableVertexAttribArray(position);
 
-  gl.vertexAttribPointer(coord, 3, gl.FLOAT, false, 0, 0); //point an attribute to the bound VBO
+ gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+ var color = gl.getAttribLocation(shaderProgram, "color");
+ gl.vertexAttribPointer(color, 3, gl.FLOAT, false,0,0) ;
+ gl.enableVertexAttribArray(color);
 
-  gl.enableVertexAttribArray(coord);
+ gl.useProgram(shaderProgram);
 
-  gl.clearColor(0.5, 0.5, 0.5, 0.9);
+//----------------------------MATRIX-------------------------------//
 
-  gl.enable(gl.DEPTH_TEST); 
+ function get_projection(angle, aspect, near, far) {
+    var ang = Math.tan((angle*.5)*Math.PI/180);//angle*.5
 
-  gl.viewport(0,0,canvas.width,canvas.height);
-  gl.drawElements(gl.TRIANGLES, indices.length,gl.UNSIGNED_SHORT,0);
+    return [
+       1/(aspect*ang), 0 , 0, 0,
+       0, 1/ang, 0, 0,
+       0, 0, -(far+near)/(far-near), -1,
+       0, 0, (-2*far*near)/(far-near), 0 
+    ];
+ }
+
+ var proj_matrix = get_projection(50, canvas.width/canvas.height, 1, 100);
+
+ var mov_matrix = [1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1];
+ var view_matrix = [1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1];
+
+ // translating z
+ view_matrix[14] = view_matrix[14] - 8;//zoom
+
+ //---------------------- ROTATE ------------------------//
+
+ function rotateX(m, angle) {
+    var c = Math.cos(angle);
+    var s = Math.sin(angle);
+
+    let m1 = m[1];
+    let m5 = m[5]; 
+    let m9 = m[9];
+
+    m[1] = c*m[1] - s*m[2]; 
+    m[2] = c*m[2] + s*m1;
+    m[5] = c*m[5] - s*m[6];
+    m[6] = c*m[6] + s*m5;
+    m[9] = c*m[9] - s*m[10];
+    m[10] = c*m[10] + s*m9;
+ }
+
+ function rotateY(m, angle) {
+    var c = Math.cos(angle);
+    var s = Math.sin(angle);
+
+    let m0 = m[0], m4 = m[4], m8 = m[8];
+
+
+    m[0] = c*m[0]+s*m[2];
+    m[2] = c*m[2]-s*m0;
+    m[4] = c*m[4]+s*m[6];
+    m[6] = c*m[6]-s*m4;
+    m[8] = c*m[8]+s*m[10];
+    m[10] = c*m[10]-s*m8
+ }
+
+ function rotateZ(m, angle) {
+  var c = Math.cos(angle);
+  var s = Math.sin(angle);
+
+  let m0 = m[0], m4 = m[4], m8 = m[8];
+
+  m[0] = c*m[0] - s*m[1];
+  m[1] = c*m[1] + s*m0;
+  m[4] = c*m[4] - s*m[5];
+  m[5] = c*m[5] + s*m4;
+  m[8] = c*m[8] - s*m[9];
+  m[9] = c*m[9] + s*m8;
 }
 
-window.onload = main;
+//------------------------ DRAW --------------------------//
+ var time_old = 0;
+
+ var animate = function(time) {
+
+    var dt = time-time_old;
+    
+    rotateX(mov_matrix, dt*0.0001*sliderX.value);
+    rotateY(mov_matrix, dt*0.0001*sliderY.value);
+    rotateZ(mov_matrix, dt*0.0001*sliderZ.value);
+
+    time_old = time;
+
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    gl.clearColor(1, 0.5, 0.5, 0.9);   //background
+    gl.clearDepth(1.0);
+
+    gl.viewport(0.0, 0.0, canvas.width, canvas.height);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    gl.uniformMatrix4fv(Pmatrix, false, proj_matrix);
+    gl.uniformMatrix4fv(Vmatrix, false, view_matrix);
+    gl.uniformMatrix4fv(Mmatrix, false, mov_matrix);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
+    gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+
+    window.requestAnimationFrame(animate);
+ }
+ animate(0);
+
